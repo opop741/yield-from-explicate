918ec102fea78be9a07b92b53ccb9a5598784ea4
Jazz_Qi <157870845@qq.com>
2019-04-25 15:48:54 +0800

    add summary.

--- yield test.py
+++ yield test.py
@@ -31,12 +31,12 @@ print(sub_gen1.send(None))
 
 '''
 总结：
-    1、return或没有下一个yield时会向一层抛出一个StopIteration异常并将return的值或None放到StopIteration第1个参数，用StopIteration.value来获取
+    1、return或没有下一个yield时会向上一层抛出一个StopIteration异常并将return的值或None放到StopIteration第1个参数，用StopIteration.value来获取
     2、委托生成器的yield from会自动将StopIteration异常的第一个参数(即子生成器return的值或None)提取出来
-    3、子生成器的return的值必须由程序员显示的在委托生成器中用return返回，不然不会向调用方返回
+    3、子生成器的return的值必须由程序员显式的在委托生成器中用return返回，不然不会向调用方返回
     4、生成器()返回的是一个类
-    5、next(生成器)与生成器.send(None)是同一回事
-    6、当子生成器终止并抛出StopIteration异常时，其异常第1参数(return或None)会返回到委托生成器的空间中，然后如果委托生成器的while仍然有效就会自动从新进入子生成器里面，并返回一个值后才会因为调用方没有进一步next(生成器)或者生成器.send(?)
+    5、next(生成器)与生成器.send(None)是同一回事，将前者了解为后者，这样比较有利于理解官方文档yield from语义的代码
+    6、当子生成器终止并抛出StopIteration异常时，其异常第1参数(return或None)会返回到委托生成器的空间中，然后如果委托生成器的while仍然有效就会自动从新进入子生成器里面，并返回一个值后才会因为调用方没有进一步next(生成器)或者生成器.send(?)而停止
 '''
 
 # def a_gen():
